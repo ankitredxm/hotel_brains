afeb59c6c1f108d394b01a1e5f3a80e6aa1dee0f
ankit kumar <anjitlol72@gmail.com>
2026-07-10 19:13:42 +0530

    security incr

--- seed.js
+++ seed.js
@@ -1,18 +1,18 @@
 const mongoose = require("mongoose");
 const Person = require("./models/person");
 
-mongoose.connect("mongodb://127.0.0.1:27017/your_db");
+mongoose.connect("mongodb+srv://ankit_db_user:Af7nZG3nYlDnJtMu@cluster0.xmgh3xm.mongodb.net/");
 
 const works = ["chef", "waiter", "manager"];
 const data = [];
 
-for (let i = 1; i <= 200; i++) {
+for (let i = 1; i <= 25; i++) {
   data.push({
     name: `Ankit ${i}`,
     age: 20 + (i % 10),
     work: works[i % works.length],
     phno: 9000000000 + i,
-    email: `ankit${i}@gmail.com`,
+    email: `ank${i}@gmail.com`,
     address: `${i} Street`,
     salary: 20000 + i * 100
   });
